e9ed1c31c78eb62febeb168cbcec8c5e4763964e
Pablo <pablo.delbarrio.arnanz@alumnos.upm.es>
2020-06-21 11:33:10 +0200

    Add security refactor

--- front/src/main/java/com/tfg/movies/front/controller/MovieController.java
+++ front/src/main/java/com/tfg/movies/front/controller/MovieController.java
@@ -5,6 +5,7 @@ import com.tfg.movies.front.service.MovieService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,35 +20,43 @@ import static java.util.concurrent.CompletableFuture.runAsync;
 @AllArgsConstructor
 public class MovieController extends BaseController {
 
-  private MovieService movieService;
+  private final MovieService movieService;
 
-  @PostMapping("/movie")
-  //@PreAuthorize("hasRole('ROLE_ADMIN')")
-  public CompletionStage<ResponseEntity<Void>> createMovie(@Valid @RequestBody Movie movie) {
-    return completeOperation(movie, movieService::createMovie);
-  }
-
-  @GetMapping("/movie/{title}")
-  //@PreAuthorize("hasRole('ROLE_VISITOR') OR hasRole('ROLE_ADMIN')")
+  @GetMapping("/no-restricted/movie/{title}")
+  @PreAuthorize("hasRole('ROLE_VISITOR') OR hasRole('ROLE_ADMIN')")
   public CompletionStage<ResponseEntity<Void>> getMovie(@PathVariable String title) {
     return completeOperation(title, movieService::getMovie);
   }
 
-  @GetMapping("/movie")
-  //@PreAuthorize("hasRole('ROLE_VISITOR') OR hasRole('ROLE_ADMIN')")
+  @GetMapping("/no-restricted/movie")
+  @PreAuthorize("hasRole('ROLE_VISITOR') OR hasRole('ROLE_ADMIN')")
   public CompletionStage<ResponseEntity<Void>> getMovies() {
     return completeOperation(Boolean.FALSE, movieService::getMovies);
   }
 
-  @DeleteMapping("/movie/{title}")
-  //@PreAuthorize("hasRole('ROLE_ADMIN')")
+  @GetMapping("/no-restricted/movie/sort")
+  @PreAuthorize("hasRole('ROLE_VISITOR') OR hasRole('ROLE_ADMIN')")
+  public CompletionStage<ResponseEntity<Void>> getSortMovies() {
+    return completeOperation(Boolean.TRUE, movieService::getMovies);
+  }
+
+  @PostMapping("/no-restricted/vote/movie")
+  @PreAuthorize("hasRole('ROLE_VISITOR') OR hasRole('ROLE_ADMIN')")
+  public CompletionStage<ResponseEntity<Void>> voteMovie(@RequestBody Movie movieToVote) {
+    return completeOperation(movieToVote, movieService::voteMovie);
+  }
+
+  //ADMIN ENDPOINTS
+  @DeleteMapping("/restricted/movie/{title}")
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
   public CompletionStage<ResponseEntity<Void>> deleteMovie(@PathVariable String title) {
     return completeOperation(title, movieService::deleteMovie);
   }
 
-  @PostMapping("/vote/movie")
-  public CompletionStage<ResponseEntity<Void>> voteMovie(@RequestBody Movie movieToVote) {
-    return completeOperation(movieToVote, movieService::voteMovie);
+  @PostMapping("/restricted/movie")
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  public CompletionStage<ResponseEntity<Void>> createMovie(@Valid @RequestBody Movie movie) {
+    return completeOperation(movie, movieService::createMovie);
   }
 
   private <T> CompletionStage<ResponseEntity<Void>> completeOperation(T payload, Consumer<T> operation) {
--- front/src/main/java/com/tfg/movies/front/security/SecurityConfig.java
+++ front/src/main/java/com/tfg/movies/front/security/SecurityConfig.java
@@ -7,29 +7,28 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-
-  protected void configure(final HttpSecurity http) throws Exception {
-    http.csrf().disable()
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
       .authorizeRequests()
-      .anyRequest()
-      .permitAll();
-    //.authenticated()
-    //.and()
-    //.formLogin()
-    //.successHandler((request, response, authentication) -> redirectStrategy.sendRedirect(request, response, "/api/movie-finder/all"));
+      .requestMatchers(req -> req.getRequestURI().contains("no-restricted"))
+      .hasAnyRole("ADMIN", "USER")
+      .requestMatchers(req -> req.getRequestURI().contains("restricted"))
+      .hasRole("ADMIN")
+      .and()
+      .httpBasic()
+      .and()
+      .csrf()
+      .disable();
   }
 
   @Bean
   public PasswordEncoder getEncoder() {
     return new BCryptPasswordEncoder();
   }
-
 }
